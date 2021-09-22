be2dcb3225be4cd6f4a7fbd40fa2923f3037c0f1
priithika <73788250+priithika@users.noreply.github.com>
2021-09-22 18:30:56 +0530

    Add files via upload

---
+++ sobeledge.m
@@ -0,0 +1,27 @@
+img = imread('[name of input image file].[file format]');
+
+img = uint8(input_image);
+figure, imshow(input_image); title('Input Image');
+
+img = rgb2gray(img);
+img = double(img);
+
+filtered_image = zeros(size(input_image));
+Mx = [-1 0 1; -2 0 2; -1 0 1];
+My = [-1 -2 -1; 0 0 0; 1 2 1];
+for i = 1:size(input_image, 1) - 2
+	for j = 1:size(input_image, 2) - 2
+		Gx = sum(sum(Mx.*input_image(i:i+2, j:j+2)));
+		Gy = sum(sum(My.*input_image(i:i+2, j:j+2)));
+					filtered_image(i+1, j+1) = sqrt(Gx.^2 + Gy.^2);
+		
+	end
+end
+filtered_image = uint8(filtered_image);
+figure, imshow(filtered_image); title('Filtered Image');
+
+thresholdValue = 100; % varies between [0 255]
+output_image = max(filtered_image, thresholdValue);
+output_image(output_image == round(thresholdValue)) = 0;
+output_image = im2bw(output_image);
+figure, imshow(output_image); title('Edge Detected Image');
